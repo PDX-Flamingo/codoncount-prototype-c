6454f7fba79cedd1b4e004d9a4d557ebe08254be
Phil Tseng <p@windowseatapp.com>
2014-06-05 20:21:37 -0700

    Switched to C++ compiler

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # the compiler: gcc for C program, define as g++ for C++
-CC = gcc
+CC = g++
 prefix=/usr/local
 exec_prefix=${prefix}
 libdir=${exec_prefix}/lib
@@ -8,7 +8,7 @@ includedir=${prefix}/include
 # compiler flags:
 #  -g    adds debugging information to the executable file
 #  -Wall turns on most, but not all, compiler warnings
-CFLAGS  = -g -Wall -I${includedir}/libcalg-1.0 -L${libdir} -lcalg -ljansson -O3 -std=c99 
+CFLAGS  = -g -I${includedir}/libcalg-1.0 -L${libdir} -lcalg -ljansson -O3
 
 # the build target executable:
 TARGET = main
--- main.c
+++ main.c
@@ -20,77 +20,9 @@
 
 #include <jansson.h>
 
-void display_codons(Trie * codoncount)
-{
-    printf("TTT: %d\n", (int) trie_lookup(codoncount, "TTT"));
-    printf("TTC: %d\n", (int) trie_lookup(codoncount, "TTC"));
-    printf("TTA: %d\n", (int) trie_lookup(codoncount, "TTA"));
-    printf("TTG: %d\n", (int) trie_lookup(codoncount, "TTG"));
-    printf("TCT: %d\n", (int) trie_lookup(codoncount, "TCT"));
-    printf("TCC: %d\n", (int) trie_lookup(codoncount, "TCC"));
-    printf("TCA: %d\n", (int) trie_lookup(codoncount, "TCA"));
-    printf("TCG: %d\n", (int) trie_lookup(codoncount, "TCG"));
-    printf("TAT: %d\n", (int) trie_lookup(codoncount, "TAT"));
-    printf("TAC: %d\n", (int) trie_lookup(codoncount, "TAC"));
-    printf("TAA: %d\n", (int) trie_lookup(codoncount, "TAA"));
-    printf("TAG: %d\n", (int) trie_lookup(codoncount, "TAG"));
-    printf("TGT: %d\n", (int) trie_lookup(codoncount, "TGT"));
-    printf("TGC: %d\n", (int) trie_lookup(codoncount, "TGC"));
-    printf("TGA: %d\n", (int) trie_lookup(codoncount, "TGA"));
-    printf("TGG: %d\n", (int) trie_lookup(codoncount, "TGG"));
-    printf("CTT: %d\n", (int) trie_lookup(codoncount, "CTT"));
-    printf("CTC: %d\n", (int) trie_lookup(codoncount, "CTC"));
-    printf("CTA: %d\n", (int) trie_lookup(codoncount, "CTA"));
-    printf("CTG: %d\n", (int) trie_lookup(codoncount, "CTG"));
-    printf("CCT: %d\n", (int) trie_lookup(codoncount, "CCT"));
-    printf("CCC: %d\n", (int) trie_lookup(codoncount, "CCC"));
-    printf("CCA: %d\n", (int) trie_lookup(codoncount, "CCA"));
-    printf("CCG: %d\n", (int) trie_lookup(codoncount, "CCG"));
-    printf("CAT: %d\n", (int) trie_lookup(codoncount, "CAT"));
-    printf("CAC: %d\n", (int) trie_lookup(codoncount, "CAC"));
-    printf("CAA: %d\n", (int) trie_lookup(codoncount, "CAA"));
-    printf("CAG: %d\n", (int) trie_lookup(codoncount, "CAG"));
-    printf("CGT: %d\n", (int) trie_lookup(codoncount, "CGT"));
-    printf("CGC: %d\n", (int) trie_lookup(codoncount, "CGC"));
-    printf("CGA: %d\n", (int) trie_lookup(codoncount, "CGA"));
-    printf("CGG: %d\n", (int) trie_lookup(codoncount, "CGG"));
-    printf("ATT: %d\n", (int) trie_lookup(codoncount, "ATT"));
-    printf("ATC: %d\n", (int) trie_lookup(codoncount, "ATC"));
-    printf("ATA: %d\n", (int) trie_lookup(codoncount, "ATA"));
-    printf("ATG: %d\n", (int) trie_lookup(codoncount, "ATG"));
-    printf("ACT: %d\n", (int) trie_lookup(codoncount, "ACT"));
-    printf("ACC: %d\n", (int) trie_lookup(codoncount, "ACC"));
-    printf("ACA: %d\n", (int) trie_lookup(codoncount, "ACA"));
-    printf("ACG: %d\n", (int) trie_lookup(codoncount, "ACG"));
-    printf("AAT: %d\n", (int) trie_lookup(codoncount, "AAT"));
-    printf("AAC: %d\n", (int) trie_lookup(codoncount, "AAC"));
-    printf("AAA: %d\n", (int) trie_lookup(codoncount, "AAA"));
-    printf("AAG: %d\n", (int) trie_lookup(codoncount, "AAG"));
-    printf("AGT: %d\n", (int) trie_lookup(codoncount, "AGT"));
-    printf("AGC: %d\n", (int) trie_lookup(codoncount, "AGC"));
-    printf("AGA: %d\n", (int) trie_lookup(codoncount, "AGA"));
-    printf("AGG: %d\n", (int) trie_lookup(codoncount, "AGG"));
-    printf("GTT: %d\n", (int) trie_lookup(codoncount, "GTT"));
-    printf("GTC: %d\n", (int) trie_lookup(codoncount, "GTC"));
-    printf("GTA: %d\n", (int) trie_lookup(codoncount, "GTA"));
-    printf("GTG: %d\n", (int) trie_lookup(codoncount, "GTG"));
-    printf("GCT: %d\n", (int) trie_lookup(codoncount, "GCT"));
-    printf("GCC: %d\n", (int) trie_lookup(codoncount, "GCC"));
-    printf("GCA: %d\n", (int) trie_lookup(codoncount, "GCA"));
-    printf("GCG: %d\n", (int) trie_lookup(codoncount, "GCG"));
-    printf("GAT: %d\n", (int) trie_lookup(codoncount, "GAT"));
-    printf("GAC: %d\n", (int) trie_lookup(codoncount, "GAC"));
-    printf("GAA: %d\n", (int) trie_lookup(codoncount, "GAA"));
-    printf("GAG: %d\n", (int) trie_lookup(codoncount, "GAG"));
-    printf("GGT: %d\n", (int) trie_lookup(codoncount, "GGT"));
-    printf("GGC: %d\n", (int) trie_lookup(codoncount, "GGC"));
-    printf("GGA: %d\n", (int) trie_lookup(codoncount, "GGA"));
-    printf("GGG: %d\n", (int) trie_lookup(codoncount, "GGG"));
-}
-
 json_t * trieValuetoJsonInteger(TrieValue i)
 {
-    return json_integer((long long) i);
+    return json_integer((long) i);
 }
 
 void save_json(Trie * counts, long int seq_length)
@@ -171,14 +103,14 @@ void save_json(Trie * counts, long int seq_length)
     
 }
 
-void count_codons(const void * sequence)
+void count_codons(const char * sequence)
 {
     const char * valid_characters = "ACTG";
     char codon[4];
     
     Trie * codoncount = trie_new();
     
-    unsigned long seq_length = strlen(sequence) - 2;
+    long seq_length = strlen(sequence) - 2;
     
     float startTime = (float)clock()/CLOCKS_PER_SEC;
     
@@ -208,15 +140,16 @@ void count_codons(const void * sequence)
             trie_insert(codoncount, codon, (TrieValue) 1);
         else
         {
-            currentcount++;
+            long current = (long) currentcount;
+            current++;
+            currentcount = (TrieValue) current;
             trie_insert(codoncount, codon, currentcount);
         }
     }
     
     float endTime = (float)clock()/CLOCKS_PER_SEC;
     float timeElapsed = (endTime - startTime);
-    
-    //display_codons(codoncount);
+
     save_json(codoncount, seq_length+2);
     
     printf("%6.6f seconds counting codons\n", timeElapsed);
@@ -229,15 +162,15 @@ void count_codons(const void * sequence)
 
 int main(int argc, const char * argv[]) {
 
-    void * testseq = "TTTTTCTTATTGTCTTCCTCATCGTATTACTAATAGTGTTGCTGATGGCTTCTCCTACTGCCTCCCCCACCGCATCACCAACAGCGTCGCCGACGGATTATCATAATGACTACCACAACGAATAACAAAAAGAGTAGCAGAAGGGTTGTCGTAGTGGCTGCCGCAGCGGATGACGAAGAGGGTGGCGGAGGGTTTTTCTTATTGTCTTCCTCATCGTATTACTAATAGTGTTGCTGATGGCTTCTCCTACTGCCTCCCCCACCGCATCACCAACAGCGTCGCCGACGGATTATCATAATGACTACCACAACGAATAACAAAAAGAGTAGCAGAAGGGTTGTCGTAGTGGCTGCCGCAGCGGATGACGAAGAGGGTGGCGGAGGGTTTTTCTTATTGTCTTCCTCATCGTATTACTAATAGTGTTGCTGATGGCTTCTCCTACTGCCTCCCCCACCGCATCACCAACAGCGTCGCCGACGGATTATCATAATGACTACCACAACGAATAACAAAAAGAGTAGCAGAAGGGTTGTCGTAGTGGCTGCCGCAGCGGATGACGAAGAGGGTGGCGGAGGGTTTTTCTTATTGTCTTCCTCATCGTATTACTAATAGTGTTGCTGATGGCTTCTCCTACTGCCTCCCCCACCGCATCACCAACAGCGTCGCCGACGGATTATCATAATGACTACCACAACGAATAACAAAAAGAGTAGCAGAAGGGTTGTCGTAGTGGCTGCCGCAGCGGATGACGAAGAGGGTGGCGGAGGGTTTTTCTTATTGTCTTCCTCATCGTATTACTAATAGTGTTGCTGATGGCTTCTCCTACTGCCTCCCCCACCGCATCACCAACAGCGTCGCCGACGGATTATCATAATGACTACCACAACGAATAACAAAAAGAGTAGCAGAAGGGTTGTCGTAGTGGCTGCCGCAGCGGATGACGAAGAGGGTGGCGGAGGG";
+    const char * testseq = "TTTTTCTTATTGTCTTCCTCATCGTATTACTAATAGTGTTGCTGATGGCTTCTCCTACTGCCTCCCCCACCGCATCACCAACAGCGTCGCCGACGGATTATCATAATGACTACCACAACGAATAACAAAAAGAGTAGCAGAAGGGTTGTCGTAGTGGCTGCCGCAGCGGATGACGAAGAGGGTGGCGGAGGGTTTTTCTTATTGTCTTCCTCATCGTATTACTAATAGTGTTGCTGATGGCTTCTCCTACTGCCTCCCCCACCGCATCACCAACAGCGTCGCCGACGGATTATCATAATGACTACCACAACGAATAACAAAAAGAGTAGCAGAAGGGTTGTCGTAGTGGCTGCCGCAGCGGATGACGAAGAGGGTGGCGGAGGGTTTTTCTTATTGTCTTCCTCATCGTATTACTAATAGTGTTGCTGATGGCTTCTCCTACTGCCTCCCCCACCGCATCACCAACAGCGTCGCCGACGGATTATCATAATGACTACCACAACGAATAACAAAAAGAGTAGCAGAAGGGTTGTCGTAGTGGCTGCCGCAGCGGATGACGAAGAGGGTGGCGGAGGGTTTTTCTTATTGTCTTCCTCATCGTATTACTAATAGTGTTGCTGATGGCTTCTCCTACTGCCTCCCCCACCGCATCACCAACAGCGTCGCCGACGGATTATCATAATGACTACCACAACGAATAACAAAAAGAGTAGCAGAAGGGTTGTCGTAGTGGCTGCCGCAGCGGATGACGAAGAGGGTGGCGGAGGGTTTTTCTTATTGTCTTCCTCATCGTATTACTAATAGTGTTGCTGATGGCTTCTCCTACTGCCTCCCCCACCGCATCACCAACAGCGTCGCCGACGGATTATCATAATGACTACCACAACGAATAACAAAAAGAGTAGCAGAAGGGTTGTCGTAGTGGCTGCCGCAGCGGATGACGAAGAGGGTGGCGGAGGG";
     
 
     int fd = open("./cow.seq", O_RDONLY);
     long long len = lseek(fd, 0, SEEK_END);
-    void *data = mmap(0, len, PROT_READ, MAP_PRIVATE, fd, 0);
+    void * data = mmap(0, len, PROT_READ, MAP_PRIVATE, fd, 0);
     
     count_codons(testseq);
-    count_codons(data);
+    count_codons((const char*) data);
     
     close(fd);
     return 0;
